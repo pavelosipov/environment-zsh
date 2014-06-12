7884c0792f4d940951f83277eca605d551f6a63f
Pavel Osipov <pavel.osipov@outlook.com>
2014-06-12 10:13:15 +0400

    Replace date as RPS1 with user@host.

--- config/prompts.zsh
+++ config/prompts.zsh
@@ -6,7 +6,7 @@
 ZSH_PROMPT_COLOR='[00;38;5;166m'
 ZSH_RESET_COLOR='[0m'
 ZSH_BASE_PS1=$'%{\e${ZSH_PROMPT_COLOR}%}%25<..<%~ %#%{\e${ZSH_RESET_COLOR}%} '
-ZSH_BASE_RPS1=$'%{\e${ZSH_PROMPT_COLOR}%}%D{%L}:%D{%M}%{\e${ZSH_RESET_COLOR}%}'
+ZSH_BASE_RPS1=$'%{\e${ZSH_PROMPT_COLOR}%}%n@%m%{\e${ZSH_RESET_COLOR}%}'
 
 PS1=$ZSH_BASE_PS1
 PS2=$'%{\e${ZSH_PROMPT_COLOR}%}%_ %{\e${ZSH_RESET_COLOR}%}'
@@ -21,24 +21,24 @@ function zle-line-init zle-keymap-select {
     zle -R
 }
 
-function zle-line-finish {
-	zle expand-history
+# function zle-line-finish {
+# 	zle expand-history
 
-	typeset last_cmd cmd hnum
-	fc -l | tail -n1 | sed -e 's/^\s\+\([0-9]\+\)\*\?\s\+\(.*\)\s*$/\1:\2/' | IFS=':' read hnum last_cmd
-	echo $BUFFER | sed -e 's/^\s\+//' | sed -e 's/\s\+$//' | read cmd
+# 	typeset last_cmd cmd hnum
+# 	fc -l | tail -n1 | sed -e 's/^\s\+\([0-9]\+\)\*\?\s\+\(.*\)\s*$/\1:\2/' | IFS=':' read hnum last_cmd
+# 	echo $BUFFER | sed -e 's/^\s\+//' | sed -e 's/\s\+$//' | read cmd
 
-	if [[ $cmd = "" ]]; then
-		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/}
-	elif [[ $cmd = $last_cmd ]]; then
-		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/'!'$hnum}
-	else
-		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/'!%h'}
-    fi
-	RPS1=''
+# 	if [[ $cmd = "" ]]; then
+# 		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/}
+# 	elif [[ $cmd = $last_cmd ]]; then
+# 		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/'!'$hnum}
+# 	else
+# 		PS1=${ZSH_BASE_PS1:s/'%25<..<%~'/'!%h'}
+#     fi
+# 	RPS1=''
 
-    zle reset-prompt
-}
+#     zle reset-prompt
+# }
 
 zle -N zle-line-init
 zle -N zle-line-finish
